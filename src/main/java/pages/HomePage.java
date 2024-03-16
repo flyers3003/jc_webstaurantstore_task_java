86b91d327cfa5a431cfe9dccd480d8946bb03f6d
John Cuthbert <flyers3003@yahoo.com>
2024-03-16 05:56:37 -0400

    Initial Submission

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,12 +1,19 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage extends BasePage{
     private static WebDriver driver;
 
+    private By homePageBanner = By.cssSelector("div[data-module='initHomePage']");
+
     public HomePage(WebDriver driver) {
         HomePage.driver = driver;
+    }
+
+    public boolean verifyHomePage(){
+        return driver.findElement(homePageBanner).isDisplayed();
     }
 }
 
