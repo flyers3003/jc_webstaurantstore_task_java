0b07da90928354b3c597d5e2975eb30f4d5881d8
John Cuthbert <flyers3003@yahoo.com>
2024-03-16 05:49:04 -0400

    Initial Submission

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,7 +6,7 @@ public class HomePage extends BasePage{
     private static WebDriver driver;
 
     public HomePage(WebDriver driver) {
-        this.driver = driver;
+        HomePage.driver = driver;
     }
 }
 
