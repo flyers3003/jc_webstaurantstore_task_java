a8bb2b67e46fba447204fca42cb1cd06f03f8bea
John Cuthbert <flyers3003@yahoo.com>
2024-03-16 05:28:31 -0400

    Initial Submission

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -3,7 +3,7 @@ package pages;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage extends BasePage{
-    private WebDriver driver;
+    private static WebDriver driver;
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
